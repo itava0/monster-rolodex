f69149b9ecf71a4c498ee3b809d09bc4b6862087
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-06-24 18:32:45 -0400

    destructured some values to make it more readable

--- src/App.js
+++ src/App.js
@@ -21,8 +21,10 @@ class App extends Component {
   };
 
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
     return (
       <div className="App">
@@ -30,7 +32,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={this.onSearchChange}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster, i) => {
           return <h1 key={i}>{monster.name}</h1>;
