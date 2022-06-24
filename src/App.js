2c56a39c82f1d029c2d0a8581e4fdc37908fb9f2
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-06-24 15:50:58 -0400

    Created a searchField that filter the monters

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
   componentDidMount() {
@@ -15,9 +16,16 @@ class App extends Component {
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    })
     return (
       <div className="App">
-       {this.state.monsters.map((monster, i) => {
+        <input className="search-box" type='search' placeholder="search monsters" onChange={(event) => {
+          const searchField = event.target.value.toLowerCase()
+          this.setState({searchField})
+        }}/>
+       {filteredMonsters.map((monster, i) => {
         return <h1 key={i}>{monster.name}</h1>
        })}
       </div>
